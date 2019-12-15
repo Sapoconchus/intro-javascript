075ed4670facb76ee3fe1cc2738e6e027b798cdd
Mario Vidal <mavidalma@gmail.com>
2019-12-15 14:25:24 +0100

    hecho romanToArab, inicio RomanChecker

--- practica/ejercicio-2-bis.js
+++ practica/ejercicio-2-bis.js
@@ -1,23 +1,26 @@
 // Probar a hacerlo dentro de una clase y cada función un método
 
+//pasar el checker antes de convertir nada en el romanToArab
+
 const romans = ["I", "V", "X", "L", "C", "D", "M"];
 const arabs = [1, 5, 10, 50, 100, 500, 1000];
 
-let romanToArab = (string) => {
+const romanToArab = (string) => {
 
     const numbersArray = [];
 
     for(let i=0; i<string.length; i++){
-       let index= romans.indexOf(string[i]); // push into numbersArray the item in romans array = index of the roman array of the string character (i)
-       let arabTransformed = arabs[index]
-       numbersArray.push(arabTransformed);
+
+      /* let index= romans.indexOf(string[i]); // push into numbersArray the item in romans array = index of the roman array of the string character (i)
+       numbersArray.push = arabs[index] }*/
+
+        numbersArray.push(arabs[romans.indexOf(string[i])]);
     }
 
-    //now we have an array of numbers ie [10, 1, 5]
+    let convertedNumber = 0; 
 
-    let convertedNumber = 0; //inicializas la variable en la que vas a ir sumando o restando el resultado del loop
-
-    for (let i = numbersArray.length; i > 0; i--) { // así va de derecha a izquierda 
+    for (let i = numbersArray.length-1; i >= 0; i--) {
+     
       if(numbersArray[i+1] === "undefined") {
           convertedNumber += numbersArray[i];
       } else if (numbersArray[i+1] > numbersArray[i]) {
@@ -25,17 +28,24 @@ let romanToArab = (string) => {
       } else {
           convertedNumber += numbersArray[i]
       }
+
     }
-}
-          
-        
-      /*  numbersArray[i] < numbersArray[i-1] && numbersArray[i] < numbersArray[i+1]) {
-        convertedNumber -= numbersArray[i]
-         } else if()
-             convertedNumber += numbersArray[i]
-            }*/
-
-    console.log(convertedNumber);
+ console.log(convertedNumber);
 }
 
+
+const romanChecker = (string) => {
+
+//metodo para ver si se repite uno de los carácteres más de tres veces ( ¿.filter?)
+// para el orden de los elementos, con los operadores [i] [i+1] 
+// ejemplo:(if arr[i] === "V/L/D" &&& arr[i+1] === "caracteres superiores") { pintar error} 
+//poner una variable "checker" como true. Si algún comprobador salta, pasarlo a false y parar el if retornando el checker.
+
+}
+
+
+romanToArab("III");
 romanToArab("XIV");
+romanToArab("XV");
+romanToArab("XVI");
+romanToArab("XX");
